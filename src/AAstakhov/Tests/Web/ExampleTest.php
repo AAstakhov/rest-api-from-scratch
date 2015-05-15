e1ed47cf9b4f2e5341fa3069db25ca05b660e819
aa <andrey.astakhov@gmail.com>
2015-05-15 23:10:28 +0200

    Added test for all example data records

--- src/AAstakhov/Tests/Web/ExampleTest.php
+++ src/AAstakhov/Tests/Web/ExampleTest.php
@@ -2,13 +2,25 @@
 
 class ExampleTest extends PHPUnit_Framework_TestCase
 {
-    public function testRequest()
+    /**
+     * @dataProvider getExistingRecordsDataProvider
+     */
+    public function testGetExistingRecords($id, array $record)
     {
         $client = new GuzzleHttp\Client();
-        $response = $client->get('http://trycatch.local/example.php/address?id=1');
 
+        $response = $client->get('http://trycatch.local/example.php/address?id=' . $id);
         $this->assertEquals('200', $response->getStatusCode());
-        $this->assertEquals(['name' => 'Marcin', 'phone' => '502145785', 'street' => 'Opata Rybickiego 1'],
-            $response->json());
+        $this->assertEquals($record, $response->json());
+    }
+
+    public function getExistingRecordsDataProvider()
+    {
+        return [
+            [0, ['name' => 'Michal', 'phone' => '506088156', 'street' => 'Michalowskiego 41']],
+            [1, ['name' => 'Marcin', 'phone' => '502145785', 'street' => 'Opata Rybickiego 1']],
+            [2, ['name' => 'Piotr', 'phone' => '504212369', 'street' => 'Horacego 23']],
+            [3, ['name' => 'Albert', 'phone' => '605458547', 'street' => 'Jan Pawła 67']]
+        ];
     }
 }
