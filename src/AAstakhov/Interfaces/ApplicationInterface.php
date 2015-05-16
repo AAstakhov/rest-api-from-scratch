5bc8d531cfa2b5fe6725314c1102257a805d6ea9
aa <andrey.astakhov@gmail.com>
2015-05-16 22:56:48 +0200

    Introduced the interface for the component Application

---
+++ src/AAstakhov/Interfaces/ApplicationInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace AAstakhov\Interfaces;
+
+interface ApplicationInterface
+{
+    /**
+     * Gets container
+     *
+     * @return ContainerInterface
+     */
+    public function getContainer();
+}
