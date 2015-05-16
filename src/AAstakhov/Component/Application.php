91a5f85d90f33e84c0f76e33b99bbca4ded196d6
aa <andrey.astakhov@gmail.com>
2015-05-16 22:57:33 +0200

    Moved Router to service container as a service. Made use of interface for Application component.

--- src/AAstakhov/Component/Application.php
+++ src/AAstakhov/Component/Application.php
@@ -2,11 +2,16 @@
 
 namespace AAstakhov\Component;
 
-class Application
+use AAstakhov\Interfaces\ApplicationInterface;
+
+class Application implements ApplicationInterface
 {
     public function getContainer()
     {
         $container = new Container();
+        $container->add('router', function () {
+            return new Router();
+        });
 
         return $container;
     }
