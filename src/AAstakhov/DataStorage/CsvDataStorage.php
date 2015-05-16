cd5febde01101f6e71bc394040abb9100f297473
aa <andrey.astakhov@gmail.com>
2015-05-16 23:24:42 +0200

    Added csv data storage class skeleton

---
+++ src/AAstakhov/DataStorage/CsvDataStorage.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AAstakhov\DataStorage;
+
+use AAstakhov\Interfaces\DataStorageInterface;
+use AAstakhov\Interfaces\RecordNotFoundException;
+
+class CsvDataStorage implements DataStorageInterface
+{
+    public function getRecord($id)
+    {
+        // TODO: Implement getRecord() method.
+    }
+
+    public function setDataSource(array $parameters)
+    {
+        // TODO: Implement setDataSource() method.
+    }
+}
