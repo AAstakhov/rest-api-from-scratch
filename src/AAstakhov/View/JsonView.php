0bd9a4f8b23cbabea251e0d91d849cdfd98efa92
aa <andrey.astakhov@gmail.com>
2015-05-17 11:26:58 +0200

    Implemented raising of ViewException when required parameter is missing.

--- src/AAstakhov/View/JsonView.php
+++ src/AAstakhov/View/JsonView.php
@@ -3,11 +3,16 @@
 namespace AAstakhov\View;
 
 use AAstakhov\Interfaces\ViewInterface;
+use AAstakhov\View\Exceptions\ViewException;
 
 class JsonView implements ViewInterface
 {
     public function render(array $parameters)
     {
+        if (!isset($parameters['record'])) {
+            throw new ViewException('Unable to render json. The parameter record is missing.');
+        }
+
         return json_encode($parameters['record']);
     }
 }
