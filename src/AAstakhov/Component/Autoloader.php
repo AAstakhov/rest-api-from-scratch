e71b6d6779818a4415b12135dc35896adfd1ff28
aa <andrey.astakhov@gmail.com>
2015-05-18 23:33:37 +0200

    Fixed path building in the Autoloader component

--- src/AAstakhov/Component/Autoloader.php
+++ src/AAstakhov/Component/Autoloader.php
@@ -12,16 +12,16 @@ class Autoloader
         if ($lastNsPos = strrpos($className, '\\')) {
             $namespace = substr($className, 0, $lastNsPos);
             $className = substr($className, $lastNsPos + 1);
-            $fileName = 'src/'.str_replace('\\', DIRECTORY_SEPARATOR, $namespace).DIRECTORY_SEPARATOR;
+            $fileName = __DIR__.'/../../'.str_replace('\\', DIRECTORY_SEPARATOR, $namespace).DIRECTORY_SEPARATOR;
         }
         $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className).'.php';
 
-        require $fileName;
+        require_once $fileName;
     }
 
     public static function register()
     {
-        spl_autoload_register(array(self, 'load'));
+        spl_autoload_register(array('self', 'load'));
     }
 
 }
